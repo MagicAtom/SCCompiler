1484eae67c9f53799a626cc7cd29cbd7b3827be4
MagicAtom <1263750383@qq.com>
2021-06-17 10:13:59 +0800

    Begin test.

---
+++ test/GenTest/Gen.h
@@ -0,0 +1,85 @@
+#ifndef SCC_GEN_H
+#define SCC_GEN_H
+
+#include "Ast.h"
+#include "Visitor.h"
+#include "Type.h"
+#include <iostream>
+#include <memory>
+#include <map>
+#include <utility>
+
+class Parser;
+class Token;
+
+class Generator:public Visitor{
+public:
+    inline Generator(){
+        context_ = std::make_unique<llvm::LLVMContext>();
+        module_ = std::make_unique<llvm::Module>("SCComplier",*context_);
+        builder_ = std::make_unique<llvm::IRBuilder<>>(*context_);
+    }
+    inline Generator(const std::vector<ASTNode*> & roots):roots_(roots){
+        context_ = std::make_unique<llvm::LLVMContext>();
+        module_ = std::make_unique<llvm::Module>("SCComplier",*context_);
+        builder_ = std::make_unique<llvm::IRBuilder<>>(*context_);
+    }
+    void GenIR();
+    void GenExec();
+    void GenObjCode(const std::string filename);
+public: // visitor
+    virtual llvm::Value* VisitBinaryOp(BinaryOp* binary) override;
+    virtual llvm::Value* VisitUnaryOp(UnaryOp* unary) override;
+    virtual llvm::Value* VisitConditionalOp(ConditionalOp* cond) override;
+    virtual llvm::Value* VisitFuncCall(FuncCall* funcCall) override;
+    virtual llvm::Value* VisitEnumerator(Enumerator* enumer) override;
+    virtual llvm::Value* VisitIdentifier(Identifier* ident) override;
+    virtual llvm::Value* VisitObject(Object* obj) override;
+    virtual llvm::Value* VisitConstant(Constant* cons) override;
+    virtual llvm::Value* VisitTempVar(TempVar* tempVar) override;
+
+    virtual llvm::Value* VisitIfStmt(IfStmt* ifStmt) override;
+    virtual llvm::Value* VisitJumpStmt(JumpStmt* jumpStmt) override;
+    virtual llvm::Value* VisitReturnStmt(ReturnStmt* returnStmt) override;
+    virtual llvm::Value* VisitLabelStmt(LabelStmt* labelStmt) override;
+    virtual llvm::Value* VisitEmptyStmt(EmptyStmt* emptyStmt) override;
+    virtual llvm::Value* VisitCompoundStmt(CompoundStmt* compStmt) override;
+    virtual llvm::Value* VisitFuncDecl(FuncDecl* funcDecl) override;
+    virtual llvm::Value* VisitDeclaration(Declaration* decl) override;
+
+private:
+    // TODO: Visual the AST here when gen IR
+private:
+    llvm::Function* GetTopFunc();
+    llvm::AllocaInst * GetInst(llvm::Function * function,llvm::StringRef VarName,llvm::Type* type);
+    llvm::Value* FindValue(const std::string name);
+    llvm::Type* TypeConvert(Type* type);
+    llvm::Type* PtrTypeConvert(Type* type);
+    llvm::Constant* InitValue(Type* type);
+
+    llvm::Value* VisitExpr(Expr* expr);
+    llvm::Value* VisitStmt(Stmt* stmt);
+    llvm::Value* GenAssignment(BinaryOp* binary);    // Binary Operator
+    llvm::Value* GenMemberRefOp(BinaryOp* binary);
+    llvm::Value* GenCommaOp(BinaryOp* binary);
+    llvm::Value* GenInc(UnaryOp* unary,bool prefix); // Unary Op
+    llvm::Value* GenCast(UnaryOp* unary);
+
+private:
+    static Parser* parser_;
+    std::vector<ASTNode*> roots_; // roots of each block
+    std::unique_ptr<llvm::Module> module_;
+    std::unique_ptr<llvm::LLVMContext> context_;
+    std::unique_ptr<llvm::IRBuilder<>> builder_;
+    std::map<std::string,llvm::Value*> name_values_;
+    std::vector<llvm::Function*> func_stack_;
+    std::vector<llvm::BasicBlock*> basic_blocks_;
+    //---------------------------------------------
+    // Below is to set register on ourselves.
+    // void EmitFunc(); // A function
+    // void EmitDecl(); // global variable
+    // Other Emit called by Emit Func() and EmitDecl()
+
+};
+
+#endif
---
+++ test/GenTest/Visitor.h
@@ -0,0 +1,70 @@
+#ifndef VISITOR_H
+#define VISITOR_h
+
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/CallingConv.h>
+#include <llvm/IR/IRPrintingPasses.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/GlobalVariable.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/IR/ValueSymbolTable.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/Interpreter.h>
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/ManagedStatic.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/DynamicLibrary.h>
+#include <llvm/Target/TargetMachine.h>
+
+class BinaryOp;
+class UnaryOp;
+class ConditionalOp;
+class FuncCall;
+class Identifier;
+class Object;
+class Enumerator;
+class Constant;
+class TempVar;
+
+class Declaration;
+class IfStmt;
+class JumpStmt;
+class ReturnStmt;
+class LabelStmt;
+class EmptyStmt;
+class CompoundStmt;
+class FuncDecl;
+class Parameter;
+
+class Visitor {
+public:
+  virtual ~Visitor() {}
+  virtual llvm::Value* VisitBinaryOp(BinaryOp* binary) = 0;
+  virtual llvm::Value* VisitUnaryOp(UnaryOp* unary) = 0;
+  virtual llvm::Value* VisitConditionalOp(ConditionalOp* cond) = 0;
+  virtual llvm::Value* VisitFuncCall(FuncCall* funcCall) = 0;
+  virtual llvm::Value* VisitEnumerator(Enumerator* enumer) = 0;
+  virtual llvm::Value* VisitIdentifier(Identifier* ident) = 0;
+  virtual llvm::Value* VisitObject(Object* obj) = 0;
+  virtual llvm::Value* VisitConstant(Constant* cons) = 0;
+  virtual llvm::Value* VisitTempVar(TempVar* tempVar) = 0;
+
+  virtual llvm::Value* VisitDeclaration(Declaration* decl) = 0;
+  virtual llvm::Value* VisitIfStmt(IfStmt* ifStmt) = 0;
+  virtual llvm::Value* VisitJumpStmt(JumpStmt* jumpStmt) = 0;
+  virtual llvm::Value* VisitReturnStmt(ReturnStmt* returnStmt) = 0;
+  virtual llvm::Value* VisitLabelStmt(LabelStmt* labelStmt) = 0;
+  virtual llvm::Value* VisitEmptyStmt(EmptyStmt* emptyStmt) = 0;
+  virtual llvm::Value* VisitCompoundStmt(CompoundStmt* compStmt) = 0;
+  virtual llvm::Value* VisitFuncDecl(FuncDecl* funcDecl) = 0;
+  virtual llvm::Value* VisitParameter(Parameter* parameter) = 0;
+};
+#endif 
